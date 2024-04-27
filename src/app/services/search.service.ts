62a08cb5e671b0996034503aae78307392de6d5c
Robert Ndonga <rgithinji@yahoo.com>
2024-04-27 08:43:53 +0300

    backend service

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, timeout } from 'rxjs';
 
-const SEARCH_API = 'http://localhost:8091/api/codes/search/';
+// const SEARCH_API = 'http://localhost:8091/api/codes/search/';
+const SEARCH_API = 'http://54.235.236.234:8091/api/codes/search/';
 
 @Injectable({
   providedIn: 'root'
